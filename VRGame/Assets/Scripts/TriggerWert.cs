812606499c8c5fafa6da89e7aa358f3a9bc8f40b
JustDrip <sebastiancremers8@gmail.com>
2022-11-13 15:24:29 +0100

    added script

---
+++ VRGame/Assets/Scripts/TriggerWert.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TriggerWert : MonoBehaviour
+{
+    public int Rechenwert;
+    public Text Scorechange;
+
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+
+            Scorechange.text = "Wert: " + Rechenwert;
+
+        }
+    }
+}
